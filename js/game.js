7037946952a0fc5f0c96c7b448fefc4269dcc0f1
albavidalm <albavidalm@gmail.com>
2022-04-14 09:52:42 +0200

    Add takecard function

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ let deck = [];
 const types = ["C", "D", "H", "S"];
 const figures = ["A", "J", "Q", "K"];
 
+//Function to create a deck and shuffle it later
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let type of types) {
@@ -15,10 +16,21 @@ const createDeck = () => {
     }
   }
 
-  console.log(deck);
   deck = _.shuffle(deck);
   console.log(deck);
   return deck;
 };
 
 createDeck();
+
+//Function to take a card
+const takeCard = () => {
+  if (deck.length === 0) {
+    throw "There are no cards left in deck";
+  }
+  const card = deck.pop();
+  console.log(deck);
+  console.log(card);
+};
+
+takeCard();
